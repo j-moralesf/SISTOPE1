6cb9432be2ba8f04bf4fa75432c58be2250b1a89
Linkoyan <65199872+Linkoyan@users.noreply.github.com>
2022-05-17 01:34:05 -0400

    vis

--- vis.c
+++ vis.c
@@ -1,113 +1,209 @@
-#include <stdio.h>
-#include <string.h>
+
+// cada hijo debe ejecutar execve
+
+// leer pipe
+
+// calcular formulas
+
+// enviar datos por el pipe
+
 #include <stdlib.h>
 #include <unistd.h>
-#include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
 
-#define DEF_LENGTH 50
-#define DEF_WORD_SIZE 100
+#define DEF_WORD_SIZE 1024
 
-
-typedef struct hijo{
-
-    int wPipe[2]; //Pipe donde el hijo escribe (w) y el padre lee.
-    int rPipe[2]; //Pipe donde el hijo lee (r) y el padre escribe.
-    int pid;      //Pid del proceso.
-    struct hijo* sig;
-}hijo;
-
-
-void addChild(hijo** childArray, hijo* child){
-
-    hijo* aux;
-
-    while( aux->sig != NULL){
-        aux= aux->sig;
-    }
-    aux->sig = child;
-}
-
-int comunicarProcesos(hijo** childArray, hijo* newHijo){
+void takeValues(char* str, double* a, double* b, double* z){
     
-    if (pipe(newHijo->wPipe) == -1){
-        printf("No se pudo crear el pipe\n");
-        exit(-1);
-    }
-
-    if(newHijo->pid = fork() == -1){
-        printf("No se pudo crear el hijo\n");
-        exit(-1);
-    }
-
-    int aPid = newHijo->pid;
-    printf("pid= %d\n", newHijo->pid);
-    if (newHijo->pid == 0){
-        printf("Soy el hijo\n");
-
-        close(newHijo->wPipe[0]); //Cierra el lado de lectura
-        close(newHijo->rPipe[1]); //Cierra el lado de escritura
-
-        if(dup2(newHijo->wPipe[1], STDOUT_FILENO) == -1){
-            printf("Error en dup2\n");
-            exit(-1);
+    int c=0;
+    int i=0;
+    int flag = 0;
+    char* num = (char*) malloc(sizeof(char)*15);
+    while(str[c] != '\0'){
+        if(str[c]== ',' || str[c] == ';'){
+            double n = atof(num);
+            if(flag == 0){
+                *a = n;
+            }
+            else if(flag == 1){
+                *b = n;
+            }
+            else if( flag == 2){
+                *z = n;
+                break;
+            }
+        
+            memset(num, 0, 15);
+            flag++;
+            i=0;
+            c++;
         }
-        if(dup2(newHijo->rPipe[0], STDIN_FILENO) == -1){
-            printf("Error en dup2\n");
-            exit(-1);
+        else{
+            num[i]= str[c];
+            i++;
+            c++;
         }
-        execlp("ls", "ls", NULL);
     }
-    else{
-        printf("Soy el padre\n");
-        close(newHijo->rPipe[0]); //Cierra el lado de lectura
-        close(newHijo->wPipe[1]); //Cierra el lado de escritura
-
-        if(dup2(newHijo->wPipe[0], STDIN_FILENO) == -1){
-            printf("Error en dup2\n");
-            exit(-1);
-        }
-        //if(dup2(newHijo->rPipe[1], STDOUT_FILENO) == -1){
-        //    printf("Error en dup2\n");
-        //    exit(-1);
-        //}
-        execlp("wc", "wc" , "-cwl", NULL);
-    }
-    addChild(childArray, newHijo);
+    free(num);
+    
 }
 
+char* separarPorLinea(char* str){
+    
+    int c =0;
+    
+    while(str[c] != '\0' || str[c] != '\n'){
 
-hijo** initializeArray(){
-
-    hijo** array = (hijo**) malloc(sizeof(hijo*)*DEF_LENGTH);
-
-    return array;
+        if( str[c] == '\n'){
+            int i=0;
+            char* aux = (char*) malloc(sizeof(char)*(c+2));
+            while(i < c){
+                aux[i] = str[i];
+                i++;
+            }       
+            aux[i]= '\0';
+            return aux;
+        }
+        c++;
+    }
+    return NULL;
 }
 
-hijo* initializeHijo(){
-
-    hijo* newHijo = (hijo*) malloc(sizeof(hijo));
-    newHijo->sig=NULL;
-    return newHijo;
+char* fixString(char* str){
+    int c =0;
+    while(str[c] != '\0' || str[c] != '\n'){
+        if( str[c] == '\n'){
+            int i=0;
+            char* aux = (char*) malloc(sizeof(char)*DEF_WORD_SIZE);
+            c++;
+            while(str[c] != '\0'){
+                aux[i] = str[c];
+                i++;
+                c++;
+            }
+            aux[i]='\0';
+            return aux;
+        }
+        c++;
+    }
+    
+    return NULL;
 }
 
+double potencia(double u, double v){
+	double res = (u*u) + (v*v);
+	return sqrt(res);
+}
 
-int main(){
+void sumarResultados(int* n, double valor_real, double valor_im, double valor_ruido, double* suma_real, double* suma_im, double* suma_pot, double* suma_ruido){
+	*suma_real = *suma_real + valor_real;
+	*suma_im = *suma_im + valor_im;
+	*suma_ruido = *suma_ruido + valor_ruido;
+	*suma_pot = *suma_pot + potencia(valor_real, valor_im);
+	*n = *n + 1;
+}
 
+void calcularMedias(int n, double suma_real, double suma_im, double* media_real, double* media_im){
+	*media_real = suma_real / n;
+	*media_im = suma_im / n;
+}
 
-    hijo** array= initializeArray();
+int main()
+{
+	int n = 0;
+	double suma_real, suma_im, suma_pot, suma_ruido;
+	double media_real, media_im, pot, ruido;
+
     int c=0;
 
-    do{
-        hijo* child = initializeHijo();
-        comunicarProcesos(array, child);
-        c++;
-        if (child->pid == 0){
+    while(1){
+
+        // Se define un buffer y variables con las que se calculan los resultados
+        
+        char* inbuff = (char*) malloc(sizeof(char)*DEF_WORD_SIZE);
+
+        double valor_real, valor_imaginario, ruido;
+
+        // Lee pipe
+
+        read(STDIN_FILENO, inbuff, sizeof(char)*DEF_WORD_SIZE);
+        fflush(STDIN_FILENO);
+     
+        FILE* aux = fopen("resultadoVis.txt", "a");
+        
+        int k = 0;
+        int flag = 0;
+
+        //fprintf(aux, "PID: %d Original inbuff:\n%s*\n", getpid(), inbuff);
+
+        while(inbuff[0] != '\0'){
+
+            char* auxS = separarPorLinea(inbuff);
+
+            if(auxS == NULL){
+                //fprintf(aux, "Error NULL\n");
+                exit(1);
+            }
+
+            // Si llega la senal de fin del padre
+            if(strcmp(auxS,"FINALIZAR")==0 || strcmp(auxS,"FINALIZAR;")==0){
+                //fprintf(aux, "lectura fin\n");
+                free(auxS);
+                flag = 1;
+                break;
+            }
+
+            fprintf(aux, "auxS: %s*\n", auxS);
+
+            inbuff = fixString(inbuff);
+            //fprintf(aux, "PID: %d FixString inbuff:\n%s*\n", getpid(), inbuff);
+
+            k++;
+            
+            takeValues(auxS, &valor_real, &valor_imaginario, &ruido);
+            
+            fprintf(aux,"%d Floats: %lf %lf %lf\n", getpid(), valor_real, valor_imaginario, ruido);
+
+            sumarResultados(&n, valor_real, valor_imaginario, ruido, &suma_real, &suma_im, &suma_pot, &suma_ruido);
+            
+            free(auxS);
+        }
+
+        memset(inbuff, 0, DEF_WORD_SIZE);
+
+        free(inbuff);
+
+
+        // Si el padre manda la senal de fin
+        if(flag==1){
+
+            fprintf(aux, "FIN DEL %d\n", getpid());
+
+            // Se calculan los valores finales del disco
+
+            calcularMedias(n, suma_real, suma_im, &media_real, &media_im);
+
+            // Se envian los resultados al pipe
+            
+            //fprintf(aux, "*n: %d\nMedia real: %lf\nMedia imaginaria: %lf\nPotencia: %lf\nRuido: %lf\n", n,media_real, media_im, suma_pot, suma_ruido);
+
+            printf("*n: %d\nMedia real: %lf\nMedia imaginaria: %lf\nPotencia: %lf\nRuido: %lf\n", n,media_real, media_im, suma_pot, suma_ruido);
+
             break;
         }
-    }while(c < 10);
+        
+        fclose(aux);
+        c++;
+    }
 
-
-    return 0;
+	return 0;
 }
+
+
+
+
 
 
